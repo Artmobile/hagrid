a3d9305f22083c1d83cfd7e0e42026dc258aa9dc
Michael Serebro <michaelserebro@gmail.com>
2012-08-20 21:26:11 +0300

    added satellite implementation

--- config.js
+++ config.js
@@ -2,6 +2,7 @@ var Config = {
 	T2_ADDRESS: 'localhost',
 	T2_EXCHANGE_NAME: 'command_exchange',
 	T1_PORT : process.env.PORT || 3000,
+	RESPONSE_TIMEOUT: 25000,
 	WORKERS:{
 		SQL: {
 			PREFETCH_COUNT:1,
--- workers/mysql-worker.js
+++ workers/mysql-worker.js
@@ -1,31 +1,22 @@
+var mysql      = require('mysql');
 var MySQLExecuter = function(args){
 	
-function Preved(){
 
-}
 	return {	
-		exec: function(onData, onError, done){
-			var mysql      = require('mysql');
+		exec: function(callback){
 			var connection = mysql.createConnection({
 			  host     	: 'localhost',
 			  database	: 'admin',
 			  user     	: 'root',
 			  password : 'sa',
 			});
-			Preved();
 			connection.connect();
 			//console.log("successfuly connected to mysql");
 			connection.query(args, function(err, rows, fields) {
-			  if (err) {
-			  	onError(err);
-			  }
-			  if(rows) {
-			  	onData(JSON.stringify({rows: rows}));
-			  }
-			  if (done) done();
-			});
-			connection.end();
-		}
+				connection.end();	
+				callback(rows, err);
+			}); //end of query
+		} //end of exec
 	} //end return
 
 
